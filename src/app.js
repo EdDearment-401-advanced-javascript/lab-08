9f04cef177b49be280096c5b8daffc669e03fb03
ekearm <ed787.ed@gmail.com>
2019-06-05 15:29:02 -0700

    added some file stuff

--- src/app.js
+++ src/app.js
@@ -6,14 +6,14 @@ const cors = require('cors');
 const morgan = require('morgan');
 
 // Esoteric Resources
-const errorHandler = require( './middleware/error.js.js');
-const notFound = require( './middleware/404.js.js' );
+const errorHandler = require( './middleware/error');
+const notFound = require( './middleware/404' );
 
 // Models
-const Products = require('./models/products.js.js');
+const Products = require('./models/products');
 const products = new Products();
 
-const Categories = require('./models/categories.js.js');
+const Categories = require('./models/categories');
 const categories = new Categories();
 
 // Prepare the express app
